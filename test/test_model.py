565da83d2027279745cc41adbecc863d1654a97b
ken_burke <kennethjburkejr@gmail.com>
2017-11-09 23:16:45 -0500

    model testing for parameter edge cases

--- test/test_model.py
+++ test/test_model.py
@@ -6,54 +6,58 @@ from math import floor
 import pytest
 import itertools
 
-param_base = {
-                                ### General Simulation Params
+@pytest.fixture
+def param_base():
+    '''Dict of parameter defaults'''
 
-    "fs" : 2e4,                 # per second
-    "sweep_length" : 1,         # seconds
-    "num_trials" : 300,         # number of simulation trials to run
-    "num_stim" : 2,             # number of AP stimuli to run
-    "stim_int" : 0.05,          # interval of AP stimuli (sec)
-    "stim1_time" : 0.1,         # time of first stimulus (sec)
+    return {
+                                    ### General Simulation Params
 
-                                ### Calcium Channel Params
+        "fs" : 2e4,                 # per second
+        "sweep_length" : 1,         # seconds
+        "num_trials" : 300,         # number of simulation trials to run
+        "num_stim" : 2,             # number of AP stimuli to run
+        "stim_int" : 0.05,          # interval of AP stimuli (sec)
+        "stim1_time" : 0.1,         # time of first stimulus (sec)
 
-    "num_syn" : 100,            # number of synapses
-    "num_cav" : 1,              # number of voltage-gated calcium channels in complex
-                                # NOTE: [Ca] comes from all channels, as if
-                                # in a cluster (but still open independently)
-    "cav_p_open" : 0.83,        # probability of CaV opening per AP
-    "cav_i" : 1,                # single CaV current per AP (arbitrary units)
-    "ca_decay" : 0.05,          # exponential decay time constant for [ca]
-    "num_cav_ratio" : 2,        # mult num_cav and divide cav_i by this
+                                    ### Calcium Channel Params
 
-                                ### Transmission / Hill Function Params
-    "vesicle_prox" : 0.25,      # Probability your vesicle is nearby the CaV complex
-    "ca_coop" : 3.72,           # Hill Function param N for calcium ion cooperativity
-                                # in vesicular release, see Scimemi & Diamond
-    "ca_ec50" : 0.7,            # Hill Function param EC_50 for calcium concentration
-                                # for half-maximal vesicle release probability                
+        "num_syn" : 100,            # number of synapses
+        "num_cav" : 1,              # number of voltage-gated calcium channels in complex
+                                    # NOTE: [Ca] comes from all channels, as if
+                                    # in a cluster (but still open independently)
+        "cav_p_open" : 0.83,        # probability of CaV opening per AP
+        "cav_i" : 1,                # single CaV current per AP (arbitrary units)
+        "ca_decay" : 0.05,          # exponential decay time constant for [ca]
+        "num_cav_ratio" : 2,        # mult num_cav and divide cav_i by this
 
-                                ### Vesicular Depletion Params
+                                    ### Transmission / Hill Function Params
+        "vesicle_prox" : 0.25,      # Probability your vesicle is nearby the CaV complex
+        "ca_coop" : 3.72,           # Hill Function param N for calcium ion cooperativity
+                                    # in vesicular release, see Scimemi & Diamond
+        "ca_ec50" : 0.7,            # Hill Function param EC_50 for calcium concentration
+                                    # for half-maximal vesicle release probability                
 
-    #probably need num vesicles in RRP? and to adjust exponential resetting func?
-    "depletion_on" : False,     # Turn on depletion?
-    "pool_tau" : 1.00,          # Readily-Releasable Pool recovery time constant.
-                                # Here, we model the RRP as a binary variable, either
-                                # occupied by one vesicle or not. Thus, we use this
-                                # to define a probability of occupancy [0,1)
+                                    ### Vesicular Depletion Params
 
-                                ### Postsynaptic Params
+        #probably need num vesicles in RRP? and to adjust exponential resetting func?
+        "depletion_on" : False,     # Turn on depletion?
+        "pool_tau" : 1.00,          # Readily-Releasable Pool recovery time constant.
+                                    # Here, we model the RRP as a binary variable, either
+                                    # occupied by one vesicle or not. Thus, we use this
+                                    # to define a probability of occupancy [0,1)
 
-    "quantal_size" : -10,       # single AMPA current per vesicle (pA)
-    "a_tau" : (0.001,0.005),    # fast and slow AMPA current time constants
+                                    ### Postsynaptic Params
+
+        "quantal_size" : -10,       # single AMPA current per vesicle (pA)
+        "a_tau" : (0.001,0.005),    # fast and slow AMPA current time constants
 
 
-                                # NOTE: EC_50 and [Ca] decay constant were constrained 
-                                #       to approximate observed changes in EPSC amplitude
-                                #       by SKF and Baclofen, given 40% reduction in
-                                #       I_cal = N_cav * p_open * cav_i       
-    }
+                                    # NOTE: EC_50 and [Ca] decay constant were constrained 
+                                    #       to approximate observed changes in EPSC amplitude
+                                    #       by SKF and Baclofen, given 40% reduction in
+                                    #       I_cal = N_cav * p_open * cav_i       
+        }
     
 
 
